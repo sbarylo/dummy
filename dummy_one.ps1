ddc521623d63d798c84c6b8544bad18a03bb393c
sbarylo <50413745+sbarylo@users.noreply.github.com>
2019-06-05 00:21:05 +0200

    Update dummy_one.ps1

--- dummy_one.ps1
+++ dummy_one.ps1
@@ -11,30 +11,27 @@ write-host ($ipmi_ips | sort -unique).count
 
 if ($ipmi_ips.count -ne ($ipmi_ips | sort -unique).count) {
 	write-host "not_equal"
-	exit 2
+	exit 1
 }
 else {
 	if ($ipmi_ips.count -lt 3) {
+		exit 2
 		write-host "less than three"
-		exit 3		
 	}
 	else {
+		$ip_octet = '(?:0?0?[0-9]|0?[1-9][0-9]|1[0-9]{2}|2[0-5][0-5]|2[0-4][0-9])'
+		[regex] $IPv4Regex = "^(?:$ip_octet\.){3}$ip_octet$"
 		foreach ($ip in $ipmi_ips) {
-			try {
-				write-host "try ip"
-				[ipaddress]$ip
-			}
-			catch {exit 4}
+				write-host "try ip" $ip
+				if (!($ip -match $IPv4Regex)){exit 3}
 		}
 #		$ipmi_ip_ping = @($ipmi_ipst | Where-Object { Test-Connection -ComputerName $_ -Quiet -Count 1})
 #		if ($ipmi_ip_ping.count -ne $ipmi_ips.count) {
-#			write-host "not all ping"
-#			exit 5
+#			exit 4
 #		}
 #		else {
 #			if(!(Test-Connection -ComputerName $env:ipmi_gw -Quiet -Count )) {
-#				write-host "gw doesn't ping"
-#				exit 6
+#				exit 5
 #			}
 #		}
 	}
